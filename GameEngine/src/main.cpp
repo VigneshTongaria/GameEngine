07df4bb2f695729cf2f4d90cc62963ef7217b2ab
Vignesh Tongaria <75326364+VigneshTongaria@users.noreply.github.com>
2025-02-02 11:33:29 +0530

    added glm package

--- GameEngine/src/main.cpp
+++ GameEngine/src/main.cpp
@@ -1,12 +1,18 @@
 #include<iostream>
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void process_inputs(GLFWwindow* window);
+void Mat_Calculations();
 
 int main()
 {
+	Mat_Calculations();
+
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
@@ -55,5 +61,17 @@ void process_inputs(GLFWwindow* window)
 	{
 		glfwSetWindowShouldClose(window, 1);
 	}
+}
+
+void Mat_Calculations()
+{
+	glm::vec4 vec(1.0f, 3.0f, 0.0f, 1.0f);
+	glm::mat4 mat_T = glm::mat4(1.0f);
+	glm::mat4 trans = glm::translate(mat_T, glm::vec3(0.0f, 6.4f, 3.0f));
+	glm::mat4 rot = glm::rotate(mat_T, 60.0f, glm::vec3(5.0f, 0.0f, 60.0f));
+	glm::mat4 scale = glm::scale(mat_T, glm::vec3(0.5f, 0.4f, 0.4f));
+
+	vec = scale*rot*trans * vec;
+	std::cout << vec.x << " "<< vec.y << " "<<vec.z << std::endl;
 }
 
