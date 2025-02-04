63976a551ed4b67ba1b1b3ef6b77aa4bfccab83f
Vignesh Tongaria <75326364+VigneshTongaria@users.noreply.github.com>
2025-02-04 22:55:13 +0530

    Used EBO and two VAOS

--- GameEngine/src/main.cpp
+++ GameEngine/src/main.cpp
@@ -92,21 +92,49 @@ int main()
 	glDeleteShader(fragment_shader);
 
 	float vertices[] = {
-	-0.5f, -0.5f, 0.0f,
-	 0.5f, -0.5f, 0.0f,
-	 0.0f,  0.5f, 0.0f
+	0.75f,  0.75f, 0.0f,  // top right
+     0.0f, -0.0f, 0.0f,  // bottom right
+    0.75f,  0.0f, 0.0f, // bottom left
 	};
-	unsigned int VBO, VAO;
+	float vertices_1[] = {
+	-0.75f,  -0.75f, 0.0f,  // top right
+     0.0f, -0.0f, 0.0f,  // bottom right
+    -0.75f,  0.0f, 0.0f, // bottom left
+	};
+	unsigned int indices[] = 
+	{
+		0,1,3, //first traingle
+		1,2,3
+	};
+	//Initializng buffers
+	unsigned int VBO, VBO_1,VAO,VAO_1,EBO;
 	glGenVertexArrays(1, &VAO);
+	glGenVertexArrays(1, &VAO_1);
 	glGenBuffers(1, &VBO);
-
+	glGenBuffers(1, &VBO_1);
+	glGenBuffers(1,&EBO);
+    
+	//Binding buffers
 	glBindVertexArray(VAO);
-
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
 
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, false, 3 * sizeof(float), (void*)0);
+
+	glEnableVertexAttribArray(0);
+
+	//glBindVertexArray(VAO_1);
+	//glBindBuffer(GL_ARRAY_BUFFER, VBO_1);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_1), vertices_1, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
+
+	glVertexAttribPointer(3,3,GL_FLOAT,false,3 * sizeof(float),(void*)3);
+
 	glEnableVertexAttribArray(0);
 
 	while (!glfwWindowShouldClose(window))
@@ -121,7 +149,13 @@ int main()
 		//draw shapes
 		glBindVertexArray(VAO);
 		glUseProgram(shader_program);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawArrays(GL_TRIANGLES,0,6);
+		//glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT,0);
+
+		// glBindVertexArray(VAO_1);
+		// glUseProgram(shader_program);
+		// glDrawArrays(GL_TRIANGLES, 0, 3);
+
 
 		//Check for all events and swaps buffers
 		glfwSwapBuffers(window);
