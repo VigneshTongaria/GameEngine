a5ea457e715e08462717ddda0f93be346a663779
Vignesh Tongaria <75326364+VigneshTongaria@users.noreply.github.com>
2025-03-03 22:52:26 +0530

    rigidbody class started

---
+++ GameEngine/src/physics/rigidbody.cpp
@@ -0,0 +1,16 @@
+#include "rigidbody.hpp"
+
+void Rigidbody::Start()
+{
+    position = &this->gameObject->position;
+}
+
+void Rigidbody::Update(float dt)
+{
+    
+}
+
+void Rigidbody::AddForce(glm::vec3 force)
+{
+
+}
--- GameEngine/src/physics/rigidbody.hpp
+++ GameEngine/src/physics/rigidbody.hpp
@@ -1,12 +1,25 @@
 #ifndef RIGIDBODY_HPP
 #define RIGIDBODY_HPP
+#include "Component.h"
+#include <glm/glm.hpp>
+#include "GameObject.h"
 
-class Rigidbody
+class Rigidbody : public Component
 {
 private:
-
+     glm::vec3* position;
 public:
-     Rigidbody();
+     float mass;
+     float velocity;
+     float accelaration;
+     Rigidbody(float mass = 1.0f,float velocity = 0.0f,float accelaration = 0.0f);
+     void AddForce(glm::vec3 force);
+
+     //Call Start after initialzing all classes and gamobjects
+     void Start();
+
+     // Call update in while loop
+     void Update(float dt);
 };
 
 
